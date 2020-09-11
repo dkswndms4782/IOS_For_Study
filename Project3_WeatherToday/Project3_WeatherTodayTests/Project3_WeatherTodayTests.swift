9e61b0dbee6f9b67ced557a9a253ef97d64860c6
dkswndms4782 <59716219+dkswndms4782@users.noreply.github.com>
2020-09-11 19:41:39 +0900

    Add files via upload

---
+++ Project3_WeatherToday/Project3_WeatherTodayTests/Project3_WeatherTodayTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project3_WeatherTodayTests.swift
+//  Project3_WeatherTodayTests
+//
+//  Created by 안유진 on 2020/08/06.
+//  Copyright © 2020 Apple inc. All rights reserved.
+//
+
+import XCTest
+@testable import Project3_WeatherToday
+
+class Project3_WeatherTodayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
